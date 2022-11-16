6fa9f0d688eb131fa9cd5200c51a2c4364ede82f
Martina Garcia <martinagg@myuax.com>
2022-11-16 12:07:46 +0100

    z

---
+++ curso1.py
@@ -0,0 +1,18 @@
+spam_amount=0
+print(spam_amount)
+spam_amount=spam_amount
+if spam_amount>0:
+    print("Yo no quiero spam")
+cancion="Spam"  * spam_amount #aqui se imprime spam tantas veces como las que tengamos de spam_amount
+
+print(5/2) la division
+print(5//2) nos da solo el primer numer
+
+alturagorro=25
+alturamia=190
+totalaltura=alturagorro+alturamia
+print( "mi altura!=" totalaltura )
+print(float(10))
+nos da el numero con decimales
+print(int(10))
+numero exacto
